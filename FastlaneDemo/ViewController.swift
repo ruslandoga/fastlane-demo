331ebd244d3c0ef6f735efe489a9dcf88d54e21d
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-09-16 00:58:05 +0300

    init

---
+++ FastlaneDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastlaneDemo
+//
+//  Created by q on 16.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ FastlaneDemoTests/FastlaneDemoTests.swift
@@ -0,0 +1,32 @@
+//
+//  FastlaneDemoTests.swift
+//  FastlaneDemoTests
+//
+//  Created by q on 16.09.2021.
+//
+
+import XCTest
+@testable import FastlaneDemo
+
+class FastlaneDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssert(1 == 1)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
